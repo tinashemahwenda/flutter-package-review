43dd5b00d7beeabb6acdfcc2ba3125972a65b555
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2022-06-08 09:58:09 +0200

    Update main.dart

--- carousel_slider_example/lib/main.dart
+++ carousel_slider_example/lib/main.dart
@@ -1,1 +1,21 @@
-import 'pa'
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyCarouselApp());
+
+class MyCarouselApp extends StatefulWidget {
+  const MyCarouselApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyCarouselApp> createState() => _MyCarouselAppState();
+}
+
+class _MyCarouselAppState extends State<MyCarouselApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Carousel Demo'),
+      ),
+    );
+  }
+}
